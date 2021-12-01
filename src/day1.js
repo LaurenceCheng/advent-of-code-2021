49cd93869b09e730f8fcda6a7a85da8ba58500d4
Chin-Yi Cheng <LaurenceCheng@users.noreply.github.com>
2021-12-02 01:21:06 +0800

    Update day1.js

--- src/day1.js
+++ src/day1.js
@@ -1,6 +1,13 @@
 const getMeasurementIncreaseTimes = (measurements) => {
-	return measurements.reduce((times, current, idx) => {
-    if (measurements[idx + 1] > measurements[idx]) times++
-  	return times
-  }, 0)  
+    return measurements.reduce((times, current, idx) => {
+        if (measurements[idx + 1] > measurements[idx]) times++
+        return times
+    }, 0)  
+}
+
+const getSumOfThreeMeasurementsIncreaseTimes = (sumOfMeasurements) => {
+    return sumOfMeasurements.reduce((times, current, idx) => {
+        if (sumOfMeasurements[idx + 3] > sumOfMeasurements[idx]) times++
+        return times
+    }, 0)  
 }
