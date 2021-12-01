28e1ff8d67f476cb6d0683db870c93e2ecad4170
Chin-Yi Cheng <LaurenceCheng@users.noreply.github.com>
2021-12-02 01:18:04 +0800

    Update day1.js

--- src/day1.js
+++ src/day1.js
@@ -1,1 +1,6 @@
-
+const getMeasurementIncreaseTimes = (measurements) => {
+	return measurements.reduce((times, current, idx) => {
+    if (measurements[idx + 1] > measurements[idx]) times++
+  	return times
+  }, 0)  
+}
