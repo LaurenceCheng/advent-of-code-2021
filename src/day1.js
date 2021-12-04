e2ba6c2613fffc454168b4dd1bd2d35dcb264af4
Chin-Yi Cheng <LaurenceCheng@users.noreply.github.com>
2021-12-05 00:15:10 +0800

    Update day1.js

--- src/day1.js
+++ src/day1.js
@@ -1,19 +1,36 @@
+const readline = require('readline')
+
+const readInput = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+  })
+
+  const output = []
+  for await (const line of rl) {
+    output.push(line)
+  }
+  return output.filter((s) => s).map(n => parseInt(n))
+}
+
 // Part 1
 const getMeasurementIncreaseTimes = (measurements) => {
-    return measurements.reduce((times, _, idx) => {
-        if (measurements[idx + 1] > measurements[idx]) times++
-        return times
-    }, 0)  
+  return measurements.reduce((times, _, idx) => {
+    if (measurements[idx + 1] > measurements[idx]) times++
+    return times
+  }, 0)
 }
-
-// getMeasurementIncreaseTimes(input)
 
 // Part 2
-const getSumOfThreeMeasurementsIncreaseTimes = (sumOfMeasurements) => {
-    return sumOfMeasurements.reduce((times, _, idx) => {
-        if (sumOfMeasurements[idx + 3] > sumOfMeasurements[idx]) times++
-        return times
-    }, 0)  
+const getSumOfThreeMeasurementsIncreaseTimes = (measurements) => {
+  return measurements.reduce((times, _, idx) => {
+    if (measurements[idx + 3] > measurements[idx]) times++
+    return times
+  }, 0)
 }
 
-// getSumOfThreeMeasurementsIncreaseTimes(input)
+;(async () => {
+  const data = await readInput()
+
+  console.log(getMeasurementIncreaseTimes(data))
+  console.log(getSumOfThreeMeasurementsIncreaseTimes(data))
+})()
