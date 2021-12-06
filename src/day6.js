f2dfc040704372eeac6192f8261851e302a9bb76
Chin-Yi Cheng <LaurenceCheng@users.noreply.github.com>
2021-12-06 13:39:06 +0800

    Create day6.js

---
+++ src/day6.js
@@ -0,0 +1,45 @@
+// in Windows, run 'type input.txt | node day4.js'
+const readline = require("readline")
+
+const readInput = async () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+  })
+
+  const output = []
+  for await (const line of rl) {
+    output.push(line)
+  }
+  return output.filter((s) => s)
+}
+
+;(async () => {
+  const data = await (await readInput()).shift()
+
+  const originalLanterns = data.split(",").map((n) => parseInt(n))
+
+  const spawnTimer = 6
+  const spawnTimerForNewlyLantern = 8
+
+  let lanternsBuckets = Array(spawnTimerForNewlyLantern + 1).fill(0)
+  lanternsBuckets = originalLanterns.reduce((buckets, currentTimer) => {
+    buckets[currentTimer]++
+    return buckets
+  }, lanternsBuckets)
+
+  const spawnDays = 256
+  for (let d = 0; d < spawnDays; d++) {
+    let numberShouldBeSpawn = 0
+    for (let i = 0; i < lanternsBuckets.length; i++) {
+      if (i === 0) numberShouldBeSpawn = lanternsBuckets[i]
+      else {
+        lanternsBuckets[i - 1] = lanternsBuckets[i]
+      }
+    }
+    lanternsBuckets[spawnTimer] += numberShouldBeSpawn
+    lanternsBuckets[spawnTimerForNewlyLantern] = numberShouldBeSpawn
+  }
+
+  const sum = lanternsBuckets.reduce((total, current) => total + current, 0)
+  console.log(sum)
+})()
